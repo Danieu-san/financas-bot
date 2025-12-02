33196ef3340457e940f59bf2295b493cd78c03e2
Daniel Santos <daniel.santos.ufrj@gmail.com>
2025-12-01 21:31:49 -0300

    Fix: Forçando versão estável 2.2413.51 do WhatsApp Web para resolver erro VERSION

--- src/services/whatsapp.js
+++ src/services/whatsapp.js
@@ -21,7 +21,7 @@ function initializeWhatsAppClient() {
         },
         webVersionCache: {
             type: 'remote',
-            remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
+            remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2413.51.html',
         }
     });
 
